5fedc874135d3b41301ce30f7c210a07da1c6602
jahnavik18 <jahnavikusuluru@gmail.com>
2026-04-11 00:17:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Prime/check-prime.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static boolean prime(int n) {
+    if(n<=1){
+        return false;
+    }
+    else{
+         for(int i=2;i<=Math.sqrt(n);i++){
+        if(n%i==0){
+            return false;
+        }
+         }
+         return true;
+    }
+  }
+}
