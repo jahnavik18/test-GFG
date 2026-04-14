caebfc4db0a92fe1476d5b16c1a602a2282bd1e3
jahnavik18 <jahnavikusuluru@gmail.com>
2026-04-14 22:46:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Encapsulation in Java/encapsulation-in-java.java
@@ -0,0 +1,20 @@
+class Person{
+    private String name;
+    private int age;
+    Person(){
+            this.name="Geeks";
+            this.age=10;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+}
