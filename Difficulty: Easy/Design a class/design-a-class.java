e6b569a6a42ee935a812e0eb7667966038e2eae5
jahnavik18 <jahnavikusuluru@gmail.com>
2026-04-23 18:28:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Design a class/design-a-class.java
@@ -0,0 +1,11 @@
+class MyClass {
+
+    public void display() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.display();
+    }
+}
