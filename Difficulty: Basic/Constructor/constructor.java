0d02086bdcfb2f1019ab5561cb66e5b3ab734731
jahnavik18 <jahnavikusuluru@gmail.com>
2026-04-23 18:26:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Constructor/constructor.java
@@ -0,0 +1,13 @@
+class User {
+
+    String name;
+
+    User() {
+        name = "Default";
+    }
+
+    public static void main(String[] args) {
+        User u = new User();
+        System.out.print(u.name);
+    }
+}
