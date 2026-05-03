db283f9be7558427a17f06e4548481e7f8d779f3
jahnavik18 <jahnavikusuluru@gmail.com>
2026-05-03 09:43:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find sum of divisors/find-sum-of-divisors.java
@@ -0,0 +1,21 @@
+// User function Template for Java
+class Solution {
+    static int sumOfDivisors(int n) {
+        // code here
+         int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    if(i%j==0)
+                    {
+                        sum=sum+j;
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+}
