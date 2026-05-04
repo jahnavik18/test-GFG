a0e8581ab1180f3ae4ce405502af8d4a3a1874bf
jahnavik18 <jahnavikusuluru@gmail.com>
2026-05-05 00:01:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -1,6 +1,5 @@
 class Solution {
     int countFreq(int[] arr, int target) {
-        // code here
         int first = findFirst(arr, target);
         if (first == -1) return 0; // Target not found
         
