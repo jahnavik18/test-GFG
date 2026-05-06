76ac292fb1fe46bfe5ce1f53b3c6bfab16925df1
jahnavik18 <jahnavikusuluru@gmail.com>
2026-05-06 23:28:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ArrayList<String> findPermutation(String s) {
+        // Code here
+        Set<String> ans = new HashSet<>();
+        genPer(s, "", ans);
+        return new ArrayList<>(ans);
+    }
+    public void genPer(String s, String a, Set<String> result) {
+        if (s.isEmpty()) {
+            result.add(a);
+            return;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            String nq = s.substring(0,i) + s.substring(i + 1);
+            genPer(nq, a + ch, result);
+        }
+    }
+}
+
