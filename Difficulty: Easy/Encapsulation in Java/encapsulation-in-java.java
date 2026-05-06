b2ec73f737b702921783c339a40a4da79ab20288
jahnavik18 <jahnavikusuluru@gmail.com>
2026-05-07 00:12:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Encapsulation in Java/encapsulation-in-java.java
+++ Difficulty: Easy/Encapsulation in Java/encapsulation-in-java.java
@@ -2,19 +2,22 @@ class Person{
     private String name;
     private int age;
     Person(){
-            this.name="Geeks";
-            this.age=10;
+        this.name="Geeks";
+        this.age=10;
+        
     }
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
+   public String  getName(){
+       return this.name;
+   }
+   public void setName(String name){
         this.name=name;
-    }
-    public int getAge(){
-        return this.age;
-    }
-    public void setAge(int age){
+   }
+   public int  getAge(){
+       return this.age;
+   }
+   public void setAge(int age){
         this.age=age;
-    }
+   }
 }
+ 
+
